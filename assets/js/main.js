d47d97a966f92278569a54626d1fa5636d611dc3
Calebe Rodrigues <calebebrodrigues@gmail.com>
2021-06-19 11:54:44 -0300

    JavaScript initial

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+(() => {
+    const inicio = document.querySelector('#inicio');
+
+    inicio.addEventListener('click', () => start());
+    
+    function start() {
+        $("#inicio").hide();
+        
+        $("#fundoGame").append("<div id='jogador'></div>");
+        $("#fundoGame").append("<div id='inimigo1'></div>");
+        $("#fundoGame").append("<div id='inimigo2'></div>");
+        $("#fundoGame").append("<div id='amigo'></div>");
+    }
+})()
+
