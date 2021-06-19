cdbbe6c449c6b9461de24bed27bbd3dc9b16ec91
Calebe Rodrigues <calebebrodrigues@gmail.com>
2021-06-19 13:16:15 -0300

    Moving background

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,10 +6,26 @@
     function start() {
         $("#inicio").hide();
         
-        $("#fundoGame").append("<div id='jogador'></div>");
-        $("#fundoGame").append("<div id='inimigo1'></div>");
+        $("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+        $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
         $("#fundoGame").append("<div id='inimigo2'></div>");
-        $("#fundoGame").append("<div id='amigo'></div>");
+        $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+
+
+        let jogo = {}
+        jogo.timer = setInterval(loop,30);
+        
+        function loop() {
+        
+            movefundo();
+        
+        } // Fim da função loop()
+
+        function movefundo() {
+            esquerda = parseInt($("#fundoGame").css("background-position"));
+            $("#fundoGame").css("background-position",esquerda-20);   
+        }
+
     }
 })()
 
